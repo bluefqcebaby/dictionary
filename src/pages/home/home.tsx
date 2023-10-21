1cf7d5a89d5047e57d2c352126c87cd687b8d08d
bluefqcebaby <bluefqcebaby@gmail.com>
2023-10-21 22:13:50 +0600

    init

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,14 @@
+import { Divider } from "@geist-ui/core";
+
+export const Home = () => {
+  return (
+    <div>
+      <header>
+        <div className="container h-10 flex justify-between items-center">
+          Dictionary
+        </div>
+        <Divider />
+      </header>
+    </div>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      center: true,
+      padding: "1rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
